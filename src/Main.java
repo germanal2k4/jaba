f2e0b04742d746a863ab8146565c229430063b8c
German Albershteyn <germanalbershteyn@MacBook-Air-German.local>
2023-11-13 10:21:14 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Введите 1 если вы хотите переводить сантиметры в дюймы и 2 если дюймы в сантиметры: ");
+        Scanner scanner = new Scanner(System.in);
+        if(scanner.hasNextInt()){
+            int number = scanner.nextInt();
+            while (!(number == 1 || number ==2)){
+                System.out.println("вы ввели не число 1 или 2");
+                number = scanner.nextInt();
+            }
+            if(number == 1){
+                System.out.println("Введите действительное число:");
+                if(scanner.hasNextDouble()){
+                    double ans = scanner.nextDouble();
+                    System.out.println(ans / 2.54);
+                    System.exit(0);
+                }
+                else{
+                    System.out.println("Вы ввели не число");
+                    System.exit(0);
+                }
+            }
+            else{
+                System.out.println("Введите действительное число");
+                if(scanner.hasNextDouble()){
+                    double ans = scanner.nextDouble();
+                    System.out.println(ans * 2.54);
+                    System.exit(0);
+                }
+                else{
+                    System.out.println("Вы ввели не число");
+                    System.exit(0);
+                }
+            }
+        }
+        if(!scanner.hasNextInt()) System.out.println("Вы ввели не число");
+    }
+}
